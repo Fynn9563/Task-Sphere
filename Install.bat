f6c9fd1fb464e39429ec06403488d61d88737f6c
Fynn9563 <mobo9563@gmail.com>
2023-11-11 23:28:44 +1100

    Implement shortcut check and update repo if exists

--- Install.bat
+++
@@ -1,8 +0,0 @@
-@echo off
-SET PowerShellScriptPath=%cd%\CreateShortcut.ps1
-
-:: Install Python requirements
-pip install -r requirements.txt
-
-:: Execute the PowerShell script
-powershell -ExecutionPolicy Bypass -NoProfile -File "%PowerShellScriptPath%"
